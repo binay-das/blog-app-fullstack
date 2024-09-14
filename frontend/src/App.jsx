2bd19d3579792fc12b97bfba123557f246384034
Binay <binaydas1123@gmail.com>
2024-09-14 19:45:42 +0530

    Initial files added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main>
+        <header>
+          <a href="" className='logo'>My Blog</a>
+          <nav>
+            <a href="">Log In</a>
+            <a href="">Register</a>
+          </nav>
+        </header>
+      </main>
+    </>
+  )
+}
+
+export default App
