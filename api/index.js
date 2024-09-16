6e6f51eceacb78541f1fc632ce59e350632357cf
Binay <binaydas1123@gmail.com>
2024-09-17 00:18:25 +0530

    Displaying all blogs

--- api/index.js
+++ api/index.js
@@ -93,6 +93,11 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
     });
     res.json({ext});
 })
+
+app.get('/post', async (req, res) => {
+    const posts = await Post.find();
+    res.json(posts);
+})
 const port = 8080;
 app.listen(port, () => {
     console.log(`Listening to port ${port}...`)
