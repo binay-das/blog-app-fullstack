cbce64e825b890366f0a4e5c6f457d0af19621d1
Binay <binaydas1123@gmail.com>
2024-09-19 15:11:20 +0530

    Image rendering done correctly

--- frontend/src/Post.jsx
+++ frontend/src/Post.jsx
@@ -7,26 +7,14 @@ export default function Post({ _id, title, summary, cover, content, createdAt, a
   return (
     <div className="post">
       <div className="image">
-        {/* <img src="https://cdn.pixabay.com/photo/2018/05/11/08/34/sky-3389832_1280.jpg" alt="" /> */}
-        {/* <img src={cover} alt="img_path" /> */}
-        {/* <img src="C:\Users\binay\Desktop\Hustle\full-stack\api\uploads\23dc7327e3aae041a2085de26d9b4414.jpg" alt="img_path" /> */}
-        {/* <img src="/assets/pic.png" alt="Cover Image" /> */}
-
-        
-          {/* <img src={'http://localhost:8080/' + cover} alt="" /> */}
-          
-            <img src={cover} alt="" />
-          
-        
+        <img src={cover} alt="cover_img" />
       </div>
 
       <div className="texts">
         <Link to={`/post/${_id}`}>
           <h2>{title}</h2>
         </Link>
-        <h2>Cover: {cover}</h2>
-        <h5>img-src: http://localhost:8080/{cover}</h5>
-        <h5>img-src(cover): {cover}</h5>
+        {/* <h2>Cover: {cover}</h2> */}
         <p className="info">
           <a href="" className='author'>Binay</a>
           {/* <a className="author">{author.username}</a> */}
